779e51d7a1717461f538bc82e1b301dfe3387881
ilya <ilya84@gmail.com>
2019-09-03 16:06:45 +0300

    figured out how to group

--- tests/main_tes.m
+++ tests/main_tes.m
@@ -6,17 +6,19 @@ hold on
 plot(X,cos(X),'linewidth',3);
 TXYL('Hi','X [um]','Y [um]','OK','No!')
 %%
+system_dependent('COM_SafeArraySingleDim', 1)
 figure1 = Powerpoint_Tunnel(gca);
 figure1.Resize;
 figure1.BackupFigure;
-% figure1.DrawLabes;
-%  figure1.DrawTitle;
-% figure1.DrawBox;
-% figure1.DrawTickValues;
-% figure1.DrawAllCurves;
+figure1.DrawLabes;
+ figure1.DrawTitle;
+figure1.DrawBox;
+figure1.DrawTickValues;
+figure1.DrawAllCurves;
 figure1.RestoreFigure;
 figure1.GeneratePNG;
 figure1.UnHide;
 figure1.InjectPNG;
+% figure1.Slide.Shapes.Range({'Rectangle 87';'Rectangle 85';'Rectangle 84'}).Group
 %%
 Ax.Children(1)
